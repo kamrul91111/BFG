906f59a614e1c3e88169fd5c3f4985aa8a8e0cb2
Otsuki <13157019@student.uts.edu.au>
2020-11-02 12:40:43 +1100

    Add project files.

---
+++ BFG/Pages/Counter.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BFG.Pages
+{
+    public partial class Counter
+    {
+        public int currentCount = 0;
+
+        public void IncrementCount() => currentCount++;
+
+    }
+}
